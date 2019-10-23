c97df055e230f7a02c617b6306917089cc3abbd7
Rafael de Sousa Magalhães <2017118050295@ifpi.int>
2019-10-23 17:15:54 -0200

    lista de produtos

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,62 @@
 import 'package:flutter/material.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final title = 'Lsta Produtos para Barba';
+    return MaterialApp(title: 'Lista de Produtos', home: Lista());
+  }
+}
 
-    return MaterialApp(
-      title: title,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: ListView(
-          children: <Widget>[
-            ListTile(
-              trailing: Icon(Icons.check_box),
-              title: Text('Pomada para barba'),
-            ),
-            ListTile(
-              trailing: Icon(Icons.check_box),
-              title: Text('Óleo para barba'),
-            ),
-            ListTile(
-              trailing: Icon(Icons.check_box),
-              title: Text('Shampoo para barba'),
-            ),
-           
-             ListTile(
-              trailing: Icon(Icons.check_box),
-              title: Text('Condicionador para barba'),
-            ),
-              ListTile(
-              trailing: Icon(Icons.check_box),
-              title: Text('Pente para barba'),
-            ),
-            ListTile(
-              trailing: Icon(Icons.check_box),
-              title: Text('Loção para barba'),
-            ),
-            ListTile(
-              trailing: Icon(Icons.check_box),
-              title: Text('Creme pós-barba'),
-            ),
-            ListTile(
-              trailing: Icon(Icons.check_box),
-              title: Text('Lamina para barbear'),
-            ),
-            ListTile(
-              trailing: Icon(Icons.check_box),
-              title: Text('Barbeador'),
-            ),
-            ListTile(
-              trailing: Icon(Icons.check_box),
-              title: Text('Hidratante para barba'),
-            ),
-          ],
-        ),
+class ListaState extends State<Lista> {
+  final _suggestions = <String>["Pomada para barba", "Shampoo para barba", "Óleo para barba", "Condicionador para barba", "Pente para barba", "Loção para barba", "Creme pós-barba", "Lamina para barbear", "Barbeador", "Hidratante para barba"];
+  final Set<String> _saved = Set<String>();
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Produtos para Barba'),
       ),
+      body: buildSuggestions(),
     );
   }
+
+  Widget buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+           return null;
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(String produto) {
+    final bool alreadySaved = _saved.contains(produto);
+    return ListTile(
+        title: Text(produto, style:_biggerFont,),
+        trailing: Icon(
+          alreadySaved ? Icons.check_box : Icons.check_box_outline_blank,
+          color: alreadySaved ? Colors.blue : null,
+        ),
+        onTap: () {
+          setState(() {
+            if (alreadySaved) {
+              _saved.remove(produto);
+            } else {
+              _saved.add(produto);
+            }
+          });
+        });
+  }
+}
+
+class Lista extends StatefulWidget {
+  @override
+  ListaState createState() => ListaState();
 }
